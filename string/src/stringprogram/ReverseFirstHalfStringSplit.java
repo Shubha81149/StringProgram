5ed71da64994fdb1ca0dc2bed76a17db381cad6b
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-25 22:50:10 +0530

    ooo

---
+++ string/src/stringprogram/ReverseFirstHalfStringSplit.java
@@ -0,0 +1,24 @@
+package stringprogram;
+
+import java.util.Scanner;
+
+public class ReverseFirstHalfStringSplit {
+
+	public static void main(String[] args) {
+
+//		Scanner sc = new Scanner(System.in);
+//		String s = sc.nextLine();
+//
+//		String[] s2 = s.split(" ");
+//		int n = (s2.length+1) / 2;
+//
+//		for (int i = n; i >= 0; i++) {
+//			System.out.print(s2[i] + " ");
+//		}
+//		for (int i = n; i < s2.length; i++) {
+//			System.out.print(s2[i]+" ");
+//			
+//		}
+	}
+
+}

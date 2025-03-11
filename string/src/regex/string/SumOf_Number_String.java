10c47242754fbe038b21e15eed5ff4177cdff881
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-03-11 11:18:00 +0530

    sumnum

---
+++ string/src/regex/string/SumOf_Number_String.java
@@ -0,0 +1,26 @@
+package regex.string;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SumOf_Number_String {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		String s = sc.next();
+		int sum = 0;
+		
+		Pattern p = Pattern.compile("[0-9]");
+		Matcher m = p.matcher(s);
+		
+		while(m.find()) {
+			
+			sum+=Integer.parseInt(m.group());
+		}
+		System.out.println("The Sum is: "+sum);
+	}
+
+}

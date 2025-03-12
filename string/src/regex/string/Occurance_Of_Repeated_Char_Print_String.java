502f9b3e70813281b0c3c5bdcc64f21a15ebcc1c
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-03-12 10:42:04 +0530

    occu

---
+++ string/src/regex/string/Occurance_Of_Repeated_Char_Print_String.java
@@ -0,0 +1,28 @@
+package regex.string;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Occurance_Of_Repeated_Char_Print_String {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		String s = sc.next();
+		String regex = "([a-z])\\1*";
+
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(s);
+		String res = "";
+		
+		while (m.find()) {
+
+			String s1 = m.group();
+			res = res+s1.length()+s1.charAt(0);  
+		}
+		System.out.println(res);
+	}
+
+}
